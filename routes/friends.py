ad32f629afa8329dca15cb6067eaf75b6a50bd7f
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2025-08-21 23:21:27 +0100

    Add endpoint for getting all of own friends

--- routes/friends.py
+++ routes/friends.py
@@ -1,1 +1,28 @@
 # Routes relating to friends
+
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
+from extensions import sdb
+
+events_bp = Blueprint('events', __name__)
+
+@events_bp.route('/friends', methods=['GET'])
+@jwt_required()
+def get_friends():
+    db = sdb.get_db()
+
+    user = get_jwt_identity()
+    user_id = f"user:{user}"
+
+    result = db.query(
+        """
+        LET $friends = SELECT * FROM relationship_with WHERE in = $user_id AND type = 'friend';
+        RETURN $friends;
+        """,
+        {"user_id": user_id}
+    )
+
+    return {
+        "friends": result
+    }, 200
