e10673c4ee2bbf1573a8f1cd73dcd022fe650a84
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2025-08-21 23:35:05 +0100

    Create endpoint for following a user

--- routes/friends.py
+++ routes/friends.py
@@ -47,6 +47,47 @@ def get_following():
         "following": result
     }, 200
 
+@social_bp.route('/following', methods=['POST'])
+@jwt_required()
+def follow_user():
+    db = sdb.get_db()
+
+    user = get_jwt_identity()
+    requester_id = f"user:{user}"
+
+    data = request.get_json()
+    target_user_id = data.get("target_user_id")
+
+    if not target_user_id:
+        return {"error": "Target user ID is required"}, 400
+    target_user_id = f"user:{target_user_id}"
+
+    # Create a following relationship
+    result = db.query(
+        """
+        IF NOT (record::exists($target_user_id)) THEN {
+            RETURN {"error": "Target user does not exist"};
+        };
+        LET $reverse_relation = SELECT * FROM relationship_with WHERE in = $target_user_id AND out = $requester_id;
+        IF $reverse_relation != [] AND $reverse_relation[0].type == 'blocked' THEN {
+            RETURN {"error": "Requester blocked by target"};
+        };
+        RELATE ONLY $requester_id->relationship_with->($target_user_id) SET type = 'following', labels = [];
+        """,
+        {"requester_id": requester_id, "target_user_id": target_user_id}
+    )
+
+    if result["error"]:
+        match result["error"]:
+            case "Target user does not exist":
+                return {"error": "Target user does not exist"}, 404
+            case "Requester blocked by target":
+                return {"error": "You have been blocked by target"}, 403
+
+    return {
+        "message": "Successfully followed user",
+        "relationship": result}, 201
+
 @social_bp.route('/followers', methods=['GET'])
 @jwt_required()
 def get_followers():
@@ -57,7 +98,7 @@ def get_followers():
 
     result = db.query(
         """
-        LET $followers = SELECT * FROM relationship_with WHERE in = $user_id AND type = 'follower';
+        LET $followers = SELECT * FROM relationship_with WHERE in = $user_id AND (type = 'follower' OR type = 'friend');
         RETURN $followers;
         """,
         {"user_id": user_id}
