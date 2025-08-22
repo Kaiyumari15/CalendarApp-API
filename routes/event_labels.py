f513f7b5ca622f03a2f07ae41debb2f2b5ae3718
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2025-08-22 09:02:55 +0100

    Add an endpoint for getting event labels

--- routes/event_labels.py
+++ routes/event_labels.py
@@ -1,1 +1,19 @@
 # Event labels related routes
+
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
+from extensions import sdb
+
+event_labels_bp = Blueprint('event-labels', __name__)
+
+@event_labels_bp.route('/event-labels', methods=['GET'])
+@jwt_required()
+def get_event_labels():
+    db = sdb.get_db()
+    # Logic to retrieve event labels
+    current_user = get_jwt_identity()
+    user_id = f"user:{current_user}"
+
+    event_labels = db.query("SELECT * FROM event_labels WHERE owner = $user_id", {"user_id": user_id})
+    return jsonify(event_labels)
