6b5a0269b81e0835c02ee1c22d0396f9b4fa96dd
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2025-08-24 20:17:57 +0100

    Add an endpoint for getting owned relationship labels

--- routes/relationship_labels.py
+++ routes/relationship_labels.py
@@ -1,1 +1,18 @@
 # Relationship labels related routes
+
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
+from extensions import sdb
+
+relationship_labels_bp = Blueprint('relationship-labels', __name__)
+
+@relationship_labels_bp.route('/', methods=['GET'])
+@jwt_required()
+def get_relationship_labels():
+    db = sdb.get_db()
+    current_user = get_jwt_identity()
+    user_id = f"user:{current_user}"
+
+    result = sdb.query("SELECT * FROM relationship_labels WHERE owner = $user_id", {"user_id": user_id})
+    return jsonify(result), 200
