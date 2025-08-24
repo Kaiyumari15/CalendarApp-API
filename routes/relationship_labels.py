1be2bf40308e5a9bf18615dff1f5a332e0147c50
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2025-08-24 21:10:53 +0100

    Implement deleting a relationship label

--- routes/relationship_labels.py
+++ routes/relationship_labels.py
@@ -51,6 +51,9 @@ def update_relationship_label(label_id):
                 IF NOT record::exists($label_id) THEN {
                     RETURN { "error": "Label not found" };
                 }
+                IF $label_id.owner != $user_id THEN {
+                    RETURN { "error": "Requester is not the owner" };
+                }
                 UPDATE $label_id MERGE $merge_data;
                 """, {"merge_data": merge_data, "label_id": label_id, "user_id": user_id})
     
@@ -58,5 +61,31 @@ def update_relationship_label(label_id):
         match result["error"]:
             case "Label not found":
                 return jsonify({"error": "Label not found"}), 404
+            case "Requester is not the owner":
+                return jsonify({"error": "Requester is not the owner"}), 403
 
     return jsonify({"message": "Label updated successfully", "label": result}), 200
+
+@relationship_labels_bp.route('/<label_id>', methods=['DELETE'])
+@jwt_required()
+def delete_relationship_label(label_id):
+    db = sdb.get_db()
+    current_user = get_jwt_identity()
+    user_id = f"user:{current_user}"
+
+    result = sdb.execute("""
+                IF NOT record::exists($label_id) THEN {
+                    RETURN { "error": "Label not found" };
+                }
+                IF $label_id.owner != $user_id THEN {
+                    RETURN { "error": "Requester is not the owner" };
+                }
+                DELETE ONLY $label_id RETURN AFTER;
+                """, {"label_id": label_id, "user_id": user_id})
+
+    if result["error"]:
+        match result["error"]:
+            case "Label not found":
+                return jsonify({"error": "Label not found"}), 404
+
+    return jsonify({"message": "Label deleted successfully"}), 200
