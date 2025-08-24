bed9bbd43c78297a88607599dfc1d8918cecd77e
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2025-08-24 21:08:51 +0100

    Implement editing a relationship label

--- routes/relationship_labels.py
+++ routes/relationship_labels.py
@@ -30,5 +30,33 @@ def create_relationship_label():
     if not name:
         return jsonify({"error": "Name is required"}), 400
 
-    sdb.execute("CREATE relationship_label SET owner = $user_id, name = $name", {"user_id": user_id, "name": name})
-    return jsonify({"message": "Label created successfully"}), 201
+    result = sdb.query("CREATE relationship_label SET owner = $user_id, name = $name RETURN AFTER", {"user_id": user_id, "name": name})
+    return jsonify({"message": "Label created successfully", "label": result}), 201
+
+@relationship_labels_bp.route('/<label_id>', methods=['PUT'])
+@jwt_required()
+def update_relationship_label(label_id):
+    db = sdb.get_db()
+    current_user = get_jwt_identity()
+    user_id = f"user:{current_user}"
+
+    data = request.get_json()
+    merge_data = data.get("merge_data")
+    if not merge_data:
+        return jsonify({"error": "Merge data is required"}), 400
+
+    name = merge_data["name"]
+
+    result = sdb.execute("""
+                IF NOT record::exists($label_id) THEN {
+                    RETURN { "error": "Label not found" };
+                }
+                UPDATE $label_id MERGE $merge_data;
+                """, {"merge_data": merge_data, "label_id": label_id, "user_id": user_id})
+    
+    if result["error"]:
+        match result["error"]:
+            case "Label not found":
+                return jsonify({"error": "Label not found"}), 404
+
+    return jsonify({"message": "Label updated successfully", "label": result}), 200
